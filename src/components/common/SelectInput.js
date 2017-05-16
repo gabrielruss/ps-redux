5fe8038be117ca8e6d0b8765cd4db87e699397f9
Gabriel <gabrielruss88@gmail.com>
2017-05-15 21:44:28 -0400

    aslkdn

--- src/components/common/SelectInput.js
+++ src/components/common/SelectInput.js
@@ -9,7 +9,8 @@ const SelectInput = ({ name, label, onChange, defaultOption, value, error, optio
                 <select
                     name={name}
                     value={value}
-                    onChange="form-control">
+                    onChange={onChange}
+                    className="form-control">
                     <option value="">{defaultOption}</option>
                     {options.map((option) => {
                         return <option key={option.value} value={option.value}>{option.text}</option>;
